ba4f40f1d9c6386c821bc224387e8d0fc1378b28
koreylombardi17 <koreylombardi17@yahoo.com>
2022-02-04 22:52:20 -0500

    Added black and white. Handled error cases.

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ camera = cv2.VideoCapture(0)
 
 while True:
     ret, frame = camera.read()
-    cv2.imshow('frame', frame) # Display frame
-    cv2.waitKey(1) # 1ms delay between each frame
+    if not ret:
+        print("Error reading frame.")
+        break
+    # Black and white frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("frame", frame)  # Display color frame
+    cv2.imshow("frame_gray", gray)  # Display black and white frame
+
+    # 1 ms between frames, enter q on keyboard to exit
+    if cv2.waitKey(1) == ord('q'):
+        break
 
 # Shut down camera, close gui
 camera.release()
 cv2.destroyAllWindows()
-
