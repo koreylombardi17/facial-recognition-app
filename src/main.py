1c84e121bc5de36d71e559dc87960ae57ba338d5
koreylombardi17 <koreylombardi17@yahoo.com>
2022-02-05 20:23:14 -0500

    Video is recording and being saved. Next step is adding facial recognition.

--- src/main.py
+++ src/main.py
@@ -1,33 +1,77 @@
 import cv2
+import time
 
 
-def display_video_color(refresh_rate_ms):
+def display_video_color():
     # Gets access to computers webcam
-    cam = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0)
+    set_cap_1080p(cap)
 
     while True:
-        ret, frame = cam.read()
+        ret, frame = cap.read()
         if not ret:
             print("Error reading frame.")
             break
 
         cv2.imshow("frame", frame)  # Display color frame
 
-        # 1 ms between frames, enter q on keyboard to exit
-        if cv2.waitKey(refresh_rate_ms) == ord('q'):
+        # Enter q on keyboard to exit, waits 1ms in between checking keystrokes
+        if cv2.waitKey(1) == ord('q'):
             break
 
     # Shut down camera, close gui
-    cam.release()
+    cap.release()
     cv2.destroyAllWindows()
 
 
-def display_video_gray(refresh_rate_ms):
+file_path = "test.avi"
+
+
+# Apples webcam fps cannot be changed and differs depending on the lighting. I found
+# 20fps to give the best results
+def record_video_color():
     # Gets access to computers webcam
-    cam = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0)
+    set_cap_1080p(cap)
+
+    # Get system's default resolution
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+
+    # Creates video writer with fourcc. fourcc is a codec that gets converted to an int. It's used to compress each frame
+    out = cv2.VideoWriter('test.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 20, (frame_width, frame_height))
+
+    initial_time = time.time()
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error reading frame.")
+            break
+
+        out.write(frame)
+        cv2.imshow("frame", frame)
+
+        # Enter q on keyboard to exit, waits 1ms in between checking keystrokes
+        if cv2.waitKey(1) == ord('q'):
+            final_time = time.time()
+            print(final_time - initial_time)
+            break
+
+    # When everything done, release the video capture and video write objects
+    cap.release()
+    out.release()
+
+    # Closes all the frames
+    cv2.destroyAllWindows()
+
+
+def display_video_gray():
+    # Gets access to computers webcam
+    cap = cv2.VideoCapture(0)
+    set_cap_1080p(cap)
 
     while True:
-        ret, frame = cam.read()
+        ret, frame = cap.read()
         if not ret:
             print("Error reading frame.")
             break
@@ -36,14 +80,28 @@ def display_video_gray(refresh_rate_ms):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imshow("frame_gray", gray)  # Display black and white frame
 
-        # 1 ms between frames, enter q on keyboard to exit
-        if cv2.waitKey(refresh_rate_ms) == ord('q'):
+        # Enter q on keyboard to exit, waits 1ms in between checking keystrokes
+        if cv2.waitKey(1) == ord('q'):
             break
 
     # Shut down camera, close gui
-    cam.release()
+    cap.release()
     cv2.destroyAllWindows()
 
 
-display_video_color(1)  # 1000 fps
-display_video_gray(100)  # 10 fps
+def set_cap_1080p(cap):
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+
+def set_cap_720p(cap):
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+
+def set_cap_480p(cap):
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+
+record_video_color()
